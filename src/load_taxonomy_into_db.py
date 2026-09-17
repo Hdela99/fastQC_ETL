69b298894b708e1316a5c535531f8bf4cb89ac80
hdela99 <hdelacruz1999@gmail.com>
2026-09-17 14:44:17 -0700

    updating to work on this on main pc as well

--- src/load_taxonomy_into_db.py
+++ src/load_taxonomy_into_db.py
@@ -3,6 +3,39 @@ import json
 import sys
 import os
 from pathlib import Path
+'''
+CREATE TABLE IF NOT EXISTS taxonomic_abundance (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    srr_id TEXT NOT NULL,
+    tax_id INTEGER,
+    name TEXT,
+    rank TEXT,
+    abundance REAL,
+    read_count INTEGER,
+    tool TEXT,
+    FOREIGN KEY (srr_id) REFERENCES runs(srr_id)
+);
+columns of the kraken_report.txt
+1: % of reads covered by clade
+2: # of reads covered by clade including descendants.
+3: # of reads assigned to that clade excluding descendants.
+4: Taxonomic rank code:
+    U: Unclassified
+    R: Root
+    D: Domain
+    K: Kingdom
+    P: Phylum
+    C: Class
+    O: Order
+    F: Family
+    G: Genus
+    S: Species
+5: NCBI Taxonomic ID num
+6: Scientific name.
+'''
+def load_taxonomy_info(srr_id, kraken_report, db_path):
+    """Extract top X ranking taxonomy reports"""
+    # Read text file
 
-def load_taxonomy_info(srr_id, fastp_json_path, db_path):
-    
+    with open(kraken_report, 'r', encoding="utf-8") as f:
+        data = f.read()
